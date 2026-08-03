a01d6c8cbb2894a360900f3cab372dd204836234
Tedo Janelidze <98310215+Tjanelidze@users.noreply.github.com>
2026-08-03 14:49:12 +0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length) return false
+
+        const isAnagram = new Array(26).fill(0);
+
+        for(let i = 0; i < s.length; i++) {
+            isAnagram[s[i].charCodeAt(0) - 'a'.charCodeAt(0)]++
+            isAnagram[t[i].charCodeAt(0) - 'a'.charCodeAt(0)]--
+        }
+
+        return isAnagram.every(c => c === 0)
+
+        // const objS: Record<string, number> = {};
+        // const objT: Record<string, number>= {};
+        // for (let i = 0; i < s.length; i++) {
+        //     objS[s[i]] = (objS[s[i]] || 0) + 1
+        //     objT[t[i]] = (objT[t[i]] || 0) + 1
+        // }
+
+        // for(s in objS) {
+        //     if(objS[s] !== objT[s]) return false
+        // }
+
+        return true
+    }
+}
