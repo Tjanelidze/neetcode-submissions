d37bc95bc19ba5baf8b6332c75cbe58f6d80af51
Tedo Janelidze <98310215+Tjanelidze@users.noreply.github.com>
2026-08-05 11:04:27 +0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            let middle = Math.floor((left + right) / 2);
+            
+            if (target === nums[middle]) {
+                return middle;
+            }
+            if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1
+    }
+}
